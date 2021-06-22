72ecc6cf8c1223ffc0a3529f35c9993da8864ca9
Joseph Mahiuha <tiasho.mahiuha@gmail.com>
2021-06-22 23:13:14 +0300

    the password problem

---
+++ day0/3-password_generator.py
@@ -0,0 +1,20 @@
+#import random module 
+#prompt user for the password
+#check if the password is comprises of lowercase, uppercase letters and symbols then print strong password
+#if the password does not meet the strong password requirements go ahead and print weak password
+
+import random
+
+def password():
+    user_input = input("Type strong for a strong password and fair for a fair password: ")
+
+    if user_input.lower() == 'fair':
+        return random.choice(['name123', 'pass12', 'memyself'])
+    else:
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_)(*&^%$%#@!~?'
+        password = ''.join(random.choices(letters, k=(random.randrange(8,24))))
+        return password
+
+print(password())
+
+

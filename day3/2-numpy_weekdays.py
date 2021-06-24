4c388ed9784fcd1917c67dae92bfc242a2ec8658
Mahiuha <tiasho.mahiuha@gmail.com>
2021-06-25 00:45:44 +0300

    added a user prompt to 3-numpy_memory.py file in day3

--- day3/2-numpy_weekdays.py
+++ day3/2-numpy_weekdays.py
@@ -1,4 +1,14 @@
 #import the numpy library
 #prompt the user for the year
 #prompt the user for the month
-#
+#print to the user the number of weekdays as output
+
+import numpy as np
+
+print('Enter From Day YYYY-MM e.g 2021-07')
+start_count = input()
+print('Enter To Day YYYY-MM e.g 2021-07')
+stop_count = input()
+s= np.busday_count(start_count, stop_count)
+
+print(s)
--- day3/3-numpy_memory.py
+++ day3/3-numpy_memory.py
@@ -1,10 +1,15 @@
+
 #import numpy
-#
+#prompt user for an array of five numbers
+#print out the memory occupied by the array back to the user as output 
 
 import numpy as np
+import random
 
-n = np.array([1, 2, 3, 4, 5])
+numbers_array = [input("Input a number: ") for i in  range(5)]
+#n = np.array([1, 2, 3, 4, 5])
+
 print("original array: ")
-print(n)
+print(numbers_array)
 print('SIze of memory occupied: ')
-print('%d bytes' %(n.size * n.itemsize))
+print('%d bytes' %(numbers_array.size * numbers_array.itemsize))

13beedb96845c334ce92288a32b74e33254e2096
Mahiuha <tiasho.mahiuha@gmail.com>
2021-06-26 09:09:10 +0300

    added some files

--- day4/6-lambda_dict.py
+++ day4/6-lambda_dict.py
@@ -1,12 +1,7 @@
-import schedule
-import time
+#a simple dictionary
+my_dict = {2: 10, 1: 2, -3: 1234}
+ 
+#sorts my_dict using lambda
+sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[0]))
+print(sorted_dict)
 
-def job():
-    print("I'm working...")
-
-schedule.every(5).seconds.do(job)
-
-
-while True:
-    schedule.run_pending()
-    time.sleep(0)
